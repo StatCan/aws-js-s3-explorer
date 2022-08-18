057964835c719fad0c7d0899ac6ec6d4ef7669d3
Collin Brown <collin.j.brown95@gmail.com>
2022-08-17 22:27:44 -0400

    refactor(sw_modify_header.js): simplify serviceworker logic

--- sw_modify_header.js
+++ sw_modify_header.js
@@ -1,11 +1,11 @@
 // Install Service Worker
 self.addEventListener('install', (e) => {
-    console.log("Service Worker: Installed");
+    console.log('Service Worker: Installed');
 })
 
 // Activate Service Worker
 self.addEventListener('activate', (e) => {
-    console.log("Service Worker: Activated");
+    console.log('Service Worker: Activated');
 })
 
 /**
@@ -16,54 +16,60 @@ self.addEventListener('activate', (e) => {
  * to automatically reject requests that have authorization headers
  * other than 'cookie' or 'x-auth-token'. To avoid significantly refactoring
  * the code for aws-js-explorer, this service worker acts as a 
- * "middleware" on the client side to modify outgoing requests from
+ * 'middleware' on the client side to modify outgoing requests from
  * the aws-js-explorer application and remove the erroneous Authorization
  * header.
  */
 self.addEventListener('fetch', (e) => {
-    if (e.request.url.includes("/standard") || e.request.url.includes("/premium")) {
-        console.log("Service Worker: s3proxy API call, modify request headers.")
-        const headers = new Headers(e.request.headers);
-        // remove erroneous authorization header
-        headers.delete("authorization");
-        if (e.request.headers.get("content-type").includes("image/") || e.request.headers.get("content-type").includes('application/octet-stream')) {
-            const promiseChain = e.request.arrayBuffer().then((originalBody) => {
-                return fetch(e.request.url, {
-                        method: e.request.method,
-                        headers,
-                        body: originalBody,
-                    });
-            });
-            e.respondWith(promiseChain);
-            return;
-        }
-        else if (e.request.headers.get("content-type").includes("text/") || e.request.headers.get("content-type").includes('application/octet-stream')) {
-            const promiseChain = e.request.text().then((originalBody) => {
-                return fetch(e.request.url, {
-                    method: e.request.method,
-                    headers,
-                    body: originalBody,
-                });
-            });
-            e.respondWith(promiseChain);
-            return;
-        }
-        else {
-            // Define new request without the Authorization header
-            var req = new Request(e.request.url, {
-                method: e.request.method,
-                headers: {
-                    ...e.request.headers
-                },
-                mode: e.request.mode,
-                credentials: e.request.credentials,
-                redirect: e.request.redirect
-            });
-            e.respondWith(fetch(req))
-        }
+    // Any requests to s3proxy need to have the erroneous authorization header removed.
+    const headers = new Headers(e.request.headers);
+    headers.delete('authorization');
+
+    // If the request is not an AJAX call to one of the s3proxy buckets, do not
+    // modify the request.
+    if (!(e.request.url.includes('/standard')) && !(e.request.url.includes('/premium'))) {
+        e.respondWith(fetch(e.request));
+    }    
+    // If the request is not a POST or a PUT request, forward the request with the
+    // modified headers
+    else if (!(e.request.method == 'PUT') && !(e.request.method == 'POST')) {
+        const req = new Request(e.request.url, {
+            headers,
+            method: e.request.method,
+            mode: e.request.mode,
+            credentials: e.request.credentials,
+            redirect: e.request.redirect
+        });
+        e.respondWith(fetch(req));
+    }
+    // If there is a PUT request where the url ends with '/', then we need to attach
+    // a '.empty' file as the request is attempting to create a directory.
+    else if (e.request.url.endsWith('/')) {
+        const emptyFile = new File([''], '.empty', { type: 'text/plain' });
+        const modifiedUrl = e.request.url + ".empty"
+        const req = new Request(modifiedUrl, {
+            headers,
+            body: emptyFile,
+            method: e.request.method,
+            mode: e.request.mode,
+            credentials: e.request.credentials,
+            redirect: e.request.redirect
+        });
+        e.respondWith(fetch(req));
     }
     else {
-        console.log("Service Worker: non-s3proxy call, do not modify request headers.")
-        e.respondWith(fetch(e.request))
+        // At this point, the user is trying to upload a file as either a single-part upload or
+        // a multi-part upload.
+        const promiseChain = e.request.arrayBuffer().then((originalBody) => {
+            return fetch(e.request.url, {
+                    headers,
+                    body: originalBody,
+                    method: e.request.method,
+                    mode: e.request.mode,
+                    credentials: e.request.credentials,
+                    redirect: e.request.redirect
+                });
+        });
+        e.respondWith(promiseChain);
     }
 });
